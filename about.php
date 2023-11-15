ac95a9b04c17576477f6a9396ec75f57074b878b
AngelCyan <133231052+AngelCyan@users.noreply.github.com>
2023-11-15 23:14:23 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,209 @@
+<?
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style1.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <title>My Personal Website</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg fixed-top">
+  <a class="navbar-brand" href="index.php">👼 Angel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fas fa-bars" style="color: #f8f8f8;"></i>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#home">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#about">About Me</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#card">Memorable Events</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#Hobbies">Hobbies</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br><br><br>
+<section id="about" class="about" data-aos="fade-up">
+  <div class="container">
+<div id="home" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/picture/bg.jpg" alt="First slide">
+    </div>
+  </div>
+</div>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <br>
+        <img src="assets/picture/1.jpg" class="img-fluid" alt="Angel's Photo">
+      </div>
+      <div class="col-lg-8 pt-4 pt-lg-0 content">
+        <br>
+        <h3>🌟 My Profile ✨</h3>
+        <div class="row">
+          <div class="col-lg-6">
+            <ul>
+              <li><i class="bi bi-rounded-right"></i> <strong>🎂 Birthday:</strong> March 1, 2003</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>🏠 Address:</strong> Labne San Miguel, Bulacan</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>✉️ Email:</strong> angellouarlantico4@gmail.com</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>📞 Phone:</strong> +639-967-913-9682</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>🫂 School Organization:</strong>Officer of Builders of Information Technology Society in Basc Chapter</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>🌍 Position:</strong> Educational and Socio-cultural Affairs</li>
+            </ul>
+          </div>
+          <div class="col-lg-6"> 
+            <ul>
+              <li><i class="bi bi-rounded-right"></i> <strong>👶 Age:</strong> 20</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>📚 School:</strong> Bulacan Agricultural State College</li>
+              <li><i class="bi bi-rounded-right"></i> <strong>🎓 Grade/Year/Section:</strong> BSIT 3B</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  <section id="card" class="card">
+	   <br>
+  <div class="container">
+    <section>
+      <h3>✨ Memorable Events in Mylife✨</h3>
+      <p class="fst-italic"></p>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/5.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">My First Student Organization</h5>
+              <p class="card-text">1st Meeting with Ma'am Melai</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/2.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+			<h5 class="card-title">First Induction</h5>
+              <p class="card-text">October 13, 2023 Induction in Bulacan Agricultural State College</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/sem.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Family Picture in Our Church</h5>
+              <p class="card-text">This Picture was taken during the Christmas Party in our Church</p>
+            </div>
+          </div>
+        </div>
+		<div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/3.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Celebrating my 19th Birthday</h5>
+              <p class="card-text">Celebrating my birthday with my friends (Incomplte)</p>
+            </div>
+          </div>
+        </div>
+		<div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/hakdog.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Hakdogers (SQUAD)</h5>
+              <p class="card-text">This is the first time that we been completed</p>
+            </div>
+          </div>
+        </div>
+		<div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/c.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Religion</h5>
+              <p class="card-text">This picture was taken on Christmas party</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/7.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Bestfriend</h5>
+              <p class="card-text">This is my bestfriend/Sister for Life.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/8.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Best Moment Ever</h5>
+              <p class="card-text">My Cat was lost but one kid saw my cat and gave it back to me.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow mx-auto" style="width: 18rem;">
+            <img class="card-img-top" src="assets/picture/9.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">RPW Friends</h5>
+              <p class="card-text">This is my first people that met in online then we celebrate our Friendship anniversary in ZEPETO.</p>
+            </div>
+          </div>
+        </div>
+
+  <section id="Hobbies" class="Hobbies">
+		<section id="Hobbies" class="container my-5">
+        <h2>🎮 Hobbies 🎬</h2>
+        <div class="row">
+            
+            <div class="col-md-4">
+                <div class="card mb-4" style="height: 100%;">
+                    <video controls muted class="card-img-top" style="height: 100%;">
+                        <source src="assets/video/f.mp4" type="video/mp4">
+                    </video>
+                    <div class="card-body">
+                        <h5>Online Games</h5>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <div class="card mb-4" style="height: 100%;">
+                    <video controls muted class="card-img-top" style="height: 100%;">
+                        <source src="assets/video/l.mp4 " type="video/mp4">
+                    </video>
+                    <div class="card-body">
+                        <h5>Editing</h5>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <div class="card mb-4" style="height: 100%;">
+                    <video controls muted class="card-img-top" style="height: 100%;">
+                        <source src="assets/video/backup.mp4" type="video/mp4">
+                    </video>
+                    <div class="card-body">
+                        <h5 class>Backup Singer</h5>
+                    </div>
+                </div>
+            </div>
+</section>
+</body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,32 @@
+<?
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-wide, personality-scale=1.0" name="viewport">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <title>Welcome to My World</title>
+</head>
+<body>
+  
+<nav class="navbar navbar-expand-lg navbar-white">
+  <a class="navbar-brand" href="index.php">👼Angel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    </ul>
+  </div>
+</nav>
+<div class="intro">
+  <h1>🌟 Angel Lou Arlantico</h1>
+  <p>Your Friendly BSIT Student</p>
+  <a href="about.php">
+    <button>Discover more about me!</button>
+  </a>
+</div>
+</body>
+</html>
